3a4d9d5795f87b1ce84cc6fcc8acd60d58e0c9f7
BuildTools <khairiissa428@gmail.com>
2018-08-26 23:18:57 -0500

    changed template

--- TileMatching/Assets/Scripts/Tile.cs
+++ TileMatching/Assets/Scripts/Tile.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public enum ArmorType { Helmet, Chest, Leggings, Boots, Gloves }
 public enum ArmorQuality { Basic, Cloth, Leather, Chainmail, Iron }
 
-public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler {
 
     public ArmorType armorType;
     public ArmorQuality armorQuality;
+    public Sprite sprite;
+    public int xID;
+    public int yID;
 
-    private Sprite sprite;
+    private bool isFlipped = false;
 
     private void Start() {
         
@@ -21,6 +25,78 @@ public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
     public void OnPointerExit(PointerEventData eventData) {
 
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        if (isFlipped) {
+            FlipTileDown();
+        }
+        else {
+            FlipTileUp();
+        }
+
+        // pass tile information to gamemanager
+        // prevent fliptiledown() while tile is the only flipped tile
+    }
+
+    public void FlipTileUp() {
+        isFlipped = true;
+        transform.GetChild(1).GetComponent<Image>().sprite = null;
+        transform.GetChild(1).GetComponent<Image>().color = Color.black;
+    }
+
+    public void FlipTileDown() {
+        isFlipped = false;
+        transform.GetChild(1).GetComponent<Image>().sprite = sprite;
+        transform.GetChild(1).GetComponent<Image>().color = Color.white;
+    }
+
+    public void SetImageSprite(ArmorQuality aq, ArmorType at) {
+        if (aq == ArmorQuality.Basic) {
+            switch (at) {
+                case ArmorType.Boots: sprite = Resources.Load<Sprite>("Sprites/sprite" + 20); break;
+                case ArmorType.Chest: sprite = Resources.Load<Sprite>("Sprites/sprite" + 1); break;
+                case ArmorType.Gloves: sprite = Resources.Load<Sprite>("Sprites/sprite" + 10); break;
+                case ArmorType.Helmet: sprite = Resources.Load<Sprite>("Sprites/sprite" + 0); break;
+                case ArmorType.Leggings: sprite = Resources.Load<Sprite>("Sprites/sprite" + 15); break;
+            }
+        }
+        else if (aq == ArmorQuality.Chainmail)   {
+            switch (at) {
+                case ArmorType.Boots: sprite = Resources.Load<Sprite>("Sprites/sprite" + 23); break;
+                case ArmorType.Chest: sprite = Resources.Load<Sprite>("Sprites/sprite" + 4); break;
+                case ArmorType.Gloves: sprite = Resources.Load<Sprite>("Sprites/sprite" + 13); break;
+                case ArmorType.Helmet: sprite = Resources.Load<Sprite>("Sprites/sprite" + 8); break;
+                case ArmorType.Leggings: sprite = Resources.Load<Sprite>("Sprites/sprite" + 18); break;
+            }
+        }
+        else if (aq == ArmorQuality.Cloth) {
+            switch (at) {
+                case ArmorType.Boots: sprite = Resources.Load<Sprite>("Sprites/sprite" + 21); break;
+                case ArmorType.Chest: sprite = Resources.Load<Sprite>("Sprites/sprite" + 2); break;
+                case ArmorType.Gloves: sprite = Resources.Load<Sprite>("Sprites/sprite" + 11); break;
+                case ArmorType.Helmet: sprite = Resources.Load<Sprite>("Sprites/sprite" + 6); break;
+                case ArmorType.Leggings: sprite = Resources.Load<Sprite>("Sprites/sprite" + 16); break;
+            }
+        }
+        else if (aq == ArmorQuality.Iron) {
+            switch (at) {
+                case ArmorType.Boots: sprite = Resources.Load<Sprite>("Sprites/sprite" + 24); break;
+                case ArmorType.Chest: sprite = Resources.Load<Sprite>("Sprites/sprite" + 5); break;
+                case ArmorType.Gloves: sprite = Resources.Load<Sprite>("Sprites/sprite" + 14); break;
+                case ArmorType.Helmet: sprite = Resources.Load<Sprite>("Sprites/sprite" + 9); break;
+                case ArmorType.Leggings: sprite = Resources.Load<Sprite>("Sprites/sprite" + 19); break;
+            }
+        }
+        else if (aq == ArmorQuality.Leather) {
+            switch (at) {
+                case ArmorType.Boots: sprite = Resources.Load<Sprite>("Sprites/sprite" + 22); break;
+                case ArmorType.Chest: sprite = Resources.Load<Sprite>("Sprites/sprite" + 3); break;
+                case ArmorType.Gloves: sprite = Resources.Load<Sprite>("Sprites/sprite" + 12); break;
+                case ArmorType.Helmet: sprite = Resources.Load<Sprite>("Sprites/sprite" + 7); break;
+                case ArmorType.Leggings: sprite = Resources.Load<Sprite>("Sprites/sprite" + 17); break;
+            }
+        }
     }
 
     public static ArmorType GetRandomArmorType() {
--- TileMatching/Assets/Scripts/TileManager.cs
+++ TileMatching/Assets/Scripts/TileManager.cs
@@ -6,7 +6,6 @@ public class TileManager : MonoBehaviour {
     public int tileCount;
 
     private GameObject tilePrefab;
-    private List<KeyValuePair<ArmorType, ArmorQuality>> pairs = new List<KeyValuePair<ArmorType, ArmorQuality>>();
     private List<GameObject> tiles = new List<GameObject>();
 
     private void Start() {
@@ -29,12 +28,11 @@ public class TileManager : MonoBehaviour {
 
     private void CreateTile(int i, ArmorType at, ArmorQuality aq) {
         GameObject tileObj = Instantiate(tilePrefab, transform);
-        Image tileImage = tileObj.transform.GetChild(1).GetComponent<Image>();
         Tile tile = tileObj.transform.GetComponent<Tile>();
 
         tile.armorType = at;
         tile.armorQuality = aq;
-        tileImage.sprite = SetImageSprite(tile.armorQuality, tile.armorType);
+        tile.SetImageSprite(tile.armorQuality, tile.armorType);
 
         tiles.Add(tileObj);
     }
@@ -47,56 +45,6 @@ public class TileManager : MonoBehaviour {
 
     private void AssignTileNeighbors() {
         
-    }
-
-    private Sprite SetImageSprite(ArmorQuality aq, ArmorType at) {
-        if (aq == ArmorQuality.Basic) {
-            switch (at) {
-                case ArmorType.Boots: return Resources.Load<Sprite>("Sprites/sprite" + 20);
-                case ArmorType.Chest: return Resources.Load<Sprite>("Sprites/sprite" + 1);
-                case ArmorType.Gloves: return Resources.Load<Sprite>("Sprites/sprite" + 10);
-                case ArmorType.Helmet: return Resources.Load<Sprite>("Sprites/sprite" + 0);
-                case ArmorType.Leggings: return Resources.Load<Sprite>("Sprites/sprite" + 15);
-            }
-        }
-        else if (aq == ArmorQuality.Chainmail) {
-            switch (at) {
-                case ArmorType.Boots: return Resources.Load<Sprite>("Sprites/sprite" + 23);
-                case ArmorType.Chest: return Resources.Load<Sprite>("Sprites/sprite" + 4);
-                case ArmorType.Gloves: return Resources.Load<Sprite>("Sprites/sprite" + 13);
-                case ArmorType.Helmet: return Resources.Load<Sprite>("Sprites/sprite" + 8);
-                case ArmorType.Leggings: return Resources.Load<Sprite>("Sprites/sprite" + 18);
-            }
-        }
-        else if (aq == ArmorQuality.Cloth) {
-            switch (at) {
-                case ArmorType.Boots: return Resources.Load<Sprite>("Sprites/sprite" + 21);
-                case ArmorType.Chest: return Resources.Load<Sprite>("Sprites/sprite" + 2);
-                case ArmorType.Gloves: return Resources.Load<Sprite>("Sprites/sprite" + 11);
-                case ArmorType.Helmet: return Resources.Load<Sprite>("Sprites/sprite" + 6);
-                case ArmorType.Leggings: return Resources.Load<Sprite>("Sprites/sprite" + 16);
-            }
-        }
-        else if (aq == ArmorQuality.Iron) {
-            switch (at) {
-                case ArmorType.Boots: return Resources.Load<Sprite>("Sprites/sprite" + 24);
-                case ArmorType.Chest: return Resources.Load<Sprite>("Sprites/sprite" + 5);
-                case ArmorType.Gloves: return Resources.Load<Sprite>("Sprites/sprite" + 14);
-                case ArmorType.Helmet: return Resources.Load<Sprite>("Sprites/sprite" + 9);
-                case ArmorType.Leggings: return Resources.Load<Sprite>("Sprites/sprite" + 19);
-            }
-        }
-        else if (aq == ArmorQuality.Leather) {
-            switch (at) {
-                case ArmorType.Boots: return Resources.Load<Sprite>("Sprites/sprite" + 22);
-                case ArmorType.Chest: return Resources.Load<Sprite>("Sprites/sprite" + 3);
-                case ArmorType.Gloves: return Resources.Load<Sprite>("Sprites/sprite" + 12);
-                case ArmorType.Helmet: return Resources.Load<Sprite>("Sprites/sprite" + 7);
-                case ArmorType.Leggings: return Resources.Load<Sprite>("Sprites/sprite" + 17);
-            }
-        }
-
-        return Resources.Load<Sprite>("Sprites/sprite" + 0);
     }
     
 }
